9b0bf47fe11b1dd5f172c1623a9e4c7b2a35b857
Maica <sunshinemaeflores3@gmail.com>
2025-04-22 00:28:27 +0800

    Update user.router.js

--- user.router.js
+++ user.router.js
@@ -1,18 +1,19 @@
-const router = require("express").Router();
-const { checkToken } = require ("../../auth/token_validation");
 const { 
     createUser, 
     getUserByUserId, 
     getUsers, 
     updateUsers, 
-    deleteUser 
+    deleteUser,
+    login 
 } = require("./user.controller");
+const router = require("express").Router();
+const { checkToken } = require ("../../auth/token_validation");
 
-router.get("/", checkToken, getUsers);
 router.post('/', checkToken, createUser);
+router.get("/", checkToken, getUsers);
 router.get("/:id", checkToken, getUserByUserId);
-router.post("/login", login);
 router.patch("/", checkToken, updateUsers);
 router.delete("/", checkToken, deleteUser);
+router.post('/login', login);
 
 module.exports = router;
