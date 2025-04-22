08cdea5ed415872833b65a82c100743405f5d512
Maica <sunshinemaeflores3@gmail.com>
2025-04-22 11:19:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./api/users/user.router");
+const cors = require('cors')
+
+app.use(cors({
+  origin: 'http://localhost:5173', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 app.use(express.json());
 
